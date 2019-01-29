9cb373626738b9e9e437f026917ee03593cdbeb8
Heonyoung Lee <>
2019-01-29 21:37:00 +0900

    Skip manyToMany and using Rest_Framework make view for image

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . import models
+from . import serializers
 
-# Create your views here.
+
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+
+        all_images = models.Image.objects.all()
+
+        images= serializers.ImageSerializers(all_images, many=True)
+
+        return Response(data=images.data)
+
